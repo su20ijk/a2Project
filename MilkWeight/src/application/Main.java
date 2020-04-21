50aa9c7709cdf56be5b78c01242e95c17871b6ba
rubben-liu <xliu859@wisc.edu>
2020-04-20 22:20:13 -0500

    1st Submission

--- MilkWeight/src/application/Main.java
+++ MilkWeight/src/application/Main.java
@@ -4,6 +4,12 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -11,11 +17,41 @@ import javafx.stage.Stage;
  *
  */
 public class Main extends Application {
-
+	
+	private static final int WINDOW_WIDTH = 800;
+	private static final int WINDOW_HEIGHT = 640;
+	private static final String APP_TITLE = "Hello World!";
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-
+		BorderPane root = new BorderPane();
+		VBox centerContainer=new VBox();
+		Label textOutputPrompt=new Label("Please enter your desired farm's ID below:");
+		textOutputPrompt.setMinSize(100, 65);
+		centerContainer.getChildren().add(textOutputPrompt);
+		
+		TextField farmIDInput=new TextField("Enter farm ID.");
+		farmIDInput.setMaxWidth(200);
+		centerContainer.getChildren().add(farmIDInput);
+		
+		Label dataTable=new Label("Date table");
+		dataTable.setMinSize(100, 200);
+		dataTable.setAlignment(Pos.CENTER);
+		centerContainer.getChildren().add(dataTable);
+		
+		TextField filePath=new TextField("File path here.");
+		filePath.setMaxWidth(100);
+		centerContainer.getChildren().add(filePath);
+		
+		centerContainer.setAlignment(Pos.CENTER);
+		root.setCenter(centerContainer);
+		
+		Scene mainScene=new Scene(root,WINDOW_WIDTH, WINDOW_HEIGHT);
+		
+		primaryStage.setTitle(APP_TITLE);
+		primaryStage.setScene(mainScene);
+		primaryStage.show();
 	}
 
 	/**
@@ -23,7 +59,7 @@ public class Main extends Application {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		launch(args);
 	}
 
 }

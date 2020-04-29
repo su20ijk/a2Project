9a0fee2bcd08b23ed27c82bc4bc3c521b5d6d7f5
Luke Mulligan <lmulligan@wisc.edu>
2020-04-28 19:25:49 -0500

    First version of internal classes

---
+++ MilkWeight/src/Internal/FarmPerMonth.java
@@ -0,0 +1,108 @@
+/**
+ * FarmPerMonth.java created by lukemulligan on MacBook Pro in MilkWeight Project
+ *
+ * Author: Luke Mulligan (lmulligan@wisc.edu)
+ * Date: @date
+ *
+ * Course: CS400
+ * Semester: Spring 2020
+ * Lecture: 001
+ *
+ * IDE: Eclipse IDE for Java Developers
+ * Version: 2019-06 (4.12.0)
+ * Build ID: 20190614-1200
+ *
+ * List Collaborators: 
+ */
+
+/**
+ * FarmPerMonth - TODO description
+ * @author lmulligan (2020)
+ *
+ */
+public class FarmPerMonth 
+{
+  private int month; // the month identifier of the FarmPerMonth
+  private String farmID; // the ID of the farm
+  private int[] weightList; // the weights of the farm for the month separated by day
+  
+  /**
+   * Constructor for the FarmPerMonth class
+   * @param farmID - the ID of the farm
+   * @param month - the month of the farm
+   * @param weightList - a list of weights by day
+   */
+  public FarmPerMonth(String farmID, int month, int[] weightList)
+  {
+    this.month = month;
+    this.farmID = farmID;
+    this.weightList = weightList;
+  }
+  
+  /**
+   * Constructor for the FarmPerMonth class
+   * @param farmID - the ID of the farm
+   * @param month - the month of the farm
+   */
+  public FarmPerMonth(String farmID, int month)
+  {
+    this.month = month;
+    this.farmID = farmID;
+    this.weightList = new int[31];
+  }
+  
+  /**
+   * @param day - the day of the weight to be returned
+   * @return the milk weight produced by the farm on a specified day
+   */
+  public int getWeightDay(int day)
+  {
+    return weightList[day-1];
+  }
+  
+  /**
+   * Add a weight on a given day
+   * @param weight - the weight to be added
+   * @param day - the day on which to add the weight
+   */
+  public void addWeight(int weight, int day)
+  {
+    weightList[day-1] = weight;
+  }
+  
+  /**
+   * @return the total weight of the month stored in the FarmPerMonth
+   */
+  public int getWeightTotal()
+  {
+    int total = 0;
+    for (int i = 0; i < weightList.length; i++)
+    {
+      total += weightList[i];
+    }
+    return total;
+  }
+  
+  /**
+   * @param sDay - the start day of the range
+   * @param eDay - the end day of the range
+   * @return the total weight over the specified range
+   */
+  public int getWeightRange(int sDay, int eDay)
+  {
+    int total = 0;
+    for (int i = sDay - 1; i < eDay; i++)
+    {
+      total += weightList[i];
+    }
+    return total;
+  }
+  
+  /**
+   * @return the ID of the farm
+   */
+  public String getID()
+  {
+    return farmID;
+  }
+}
---
+++ MilkWeight/src/Internal/MilkWeightTest.java
@@ -0,0 +1,56 @@
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * MilkWeightTest.java created by lukemulligan on MacBook Pro in MilkWeight Project
+ *
+ * Author: Luke Mulligan (lmulligan@wisc.edu)
+ * Date: @date
+ *
+ * Course: CS400
+ * Semester: Spring 2020
+ * Lecture: 001
+ *
+ * IDE: Eclipse IDE for Java Developers
+ * Version: 2019-06 (4.12.0)
+ * Build ID: 20190614-1200
+ *
+ * List Collaborators: 
+ */
+
+/**
+ * MilkWeightTest - TODO description
+ * @author lmulligan (2020)
+ *
+ */
+class MilkWeightTest
+  {
+    private Main main;
+    
+    @BeforeEach
+    void setUp() throws Exception 
+    {
+      main = new Main();
+    }
+    
+    @Test
+    void test001_read_file() 
+    {
+      main.readFile(new File("2019-1.csv"));
+      main.readFile(new File("2019-2.csv"));
+      main.readFile(new File("2019-3.csv"));
+      int[] farmReport = main.farmReport("0", 2019);
+      Map<String, Integer> yearReport = main.yearReport(2019);
+      Map<String, Integer> monthReport = main.monthReport(2019, 1);
+      Map<String, Integer> yearReportSorted = main.sortAscendingWeight(yearReport);
+      Map<String, Integer> dateRangeReport = main.dateRangeReport(2019, 1, 8, 3, 18);
+      double[] percents = main.percentList(yearReport);
+      System.out.println(yearReportSorted);
+      System.out.println(dateRangeReport);
+    }
+
+  }
+
